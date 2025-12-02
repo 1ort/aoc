945d5f3d134fc99f6cd4cd11f36987b4261f9140
1ort <83316072+1ort@users.noreply.github.com>
2025-12-02 20:25:52 +0500

    d2p2

--- 2025/day2/solution.py
+++ 2025/day2/solution.py
@@ -5,6 +5,18 @@ def is_invalid_id(id_: int) -> bool:
     return x[:len(x)//2] == x[len(x)//2:]
 
 
+def is_invalid_id_by_multiple_len(id_: int) -> bool:
+    x = str(id_)
+    for l in range(1, len(x)):
+        fragments = [x[i:i+l] for i in range(0, len(x), l)]
+        if len(set(fragments)) == 1:
+            print(fragments)
+            return True
+    return False
+
+
+
+
 result = 0
 
 with open('input.txt') as f:
@@ -15,6 +27,6 @@ with open('input.txt') as f:
         begin, end = range_.split('-')
         print(begin, end)
         for i in range(int(begin), int(end)+1):
-            if is_invalid_id(i):
+            if is_invalid_id_by_multiple_len(i):
                 result+=i
 print(result)
