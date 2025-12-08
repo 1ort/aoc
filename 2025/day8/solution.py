0adb157ea618afdae4999677dbf06ae2098b92fd
1ort <83316072+1ort@users.noreply.github.com>
2025-12-08 16:54:30 +0500

    d8p2

--- 2025/day8/solution.py
+++ 2025/day8/solution.py
@@ -22,37 +22,54 @@ for i, point_a in enumerate(points):
             (squared_dist(point_a, point_b), point_a, point_b)
         )
 
-dsu = {point: point for point in points}
+class DSU:
+    """disjoint set union""" 
 
-def dsu_find(point):
-    while point != dsu[point]:
-        dsu[point] = dsu[dsu[point]]
-        point = dsu[point]
-    return point
+    def __init__(self, points) -> None:
+        self.dsu = {point: point for point in points}
 
-def dsu_union(a, b):
-    parent_a, parent_b = dsu_find(a), dsu_find(b)
-    if parent_a != parent_b:
-        dsu[parent_a] = parent_b
+    def find(self, point):
+        while point != self.dsu[point]:
+            self.dsu[point] = self.dsu[self.dsu[point]]
+            point = self.dsu[point]
+        return point
 
-def dsu_connected(a, b):
-    return dsu_find(a) == dsu_find(b)
+    def union(self, a, b):
+        parent_a, parent_b = self.find(a), self.find(b)
+        if parent_a != parent_b:
+            self.dsu[parent_a] = parent_b
 
+    def connected(self, a, b):
+        return self.find(a) == self.find(b)
+pt2_squared_distances = squared_distances.copy()
+
+dsu = DSU(points)
 
 for i in range(1000):
     _, point_a, point_b = heapq.heappop(squared_distances)
-    if dsu_connected(point_a, point_b):
+    if dsu.connected(point_a, point_b):
         continue
-    dsu_union(point_a, point_b)
+    dsu.union(point_a, point_b)
+    last_connected = (point_a, point_b)
 
 dsu_circuits = defaultdict(set)
-for elem in dsu:
-    dsu_circuits[dsu_find(elem)].add(elem)
+for elem in dsu.dsu:
+    dsu_circuits[dsu.find(elem)].add(elem)
 
 circuit_lens = sorted((len(circuit) for circuit in dsu_circuits.values()), reverse=True)
 
 print('pt1:', reduce(operator.mul, circuit_lens[:3]))
 
+last_connected = None
+dsu = DSU(points)
+while pt2_squared_distances:
+    _, point_a, point_b = heapq.heappop(pt2_squared_distances)
+    if dsu.connected(point_a, point_b):
+        continue
+    dsu.union(point_a, point_b)
+    last_connected = (point_a, point_b)
+
+print('pt2:', last_connected[0][0] * last_connected[1][0])
 
 
 
